d04cafce110a3aeb3aeeeeb199c99bbe5f057a4d
Abbasi <moabbasi@teksystems.com>
2018-11-15 11:38:54 +0530

    changed 3

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-'''import requests
-from git import Repo
-#from os import getcwd
-
-
-		
-url = "https://raw.githubusercontent.com/farmanAbbasi/helloWorldJenkins/master/folder1/f1_text2.txt"
-#directory = getcwd()
-filename = 'C:/Users/moabbasi/Desktop/gitFiles/Test1.txt'
-r = requests.get(url)
-content=r.content.decode('ascii')
-
-f = open(filename,'w')
-f.write(content)
-print(r.content)'''
 import requests
 import os
 from git import Repo
-
-
 COMMITS_TO_PRINT = 1
 
 
@@ -29,20 +12,15 @@ def print_commit(commit):
     temp=my_string.split("diff --git a")[1]
     temp2=temp.split(" ",1)[0]
     print(temp2)
+    print(type(temp2))   
     url = "https://raw.githubusercontent.com/farmanAbbasi/helloWorldJenkins/master"+temp2
     filename = 'C:/Users/moabbasi/Desktop/gitFiles/Test1.txt'
     r = requests.get(url)
     content=r.content.decode('ascii')
-
     f = open(filename,'w')
     f.write(content)
-    print(r.content)
-    #print(str(repo.git.diff(t)))
-    #print("\"{}\" by {} ({})".format(commit.summary,commit.author.name,commit.author.email))
     print(str(commit.authored_datetime))
-    #print(str("count: {} and size: {}".format(commit.count(),commit.size)))
-											 
-
+   											
 
 def print_repository(repo):
     print('Repo description: {}'.format(repo.description))
@@ -55,13 +33,10 @@ def print_repository(repo):
 if __name__ == "__main__":
     repo_path = os.getenv('https://github.com/farmanAbbasi/helloWorldJenkins')
     #repo = git.Repo(os.path.dirname())
-    # Repo object used to programmatically interact with Git repositories
     repo = Repo(repo_path,search_parent_directories=True)
-    # check that the repository loaded correctly
     if not repo.bare:
         print('Repo at {} successfully loaded.'.format(repo_path))
         print_repository(repo)
-        # create list of commits then print some of them to stdout
         commits = list(repo.iter_commits('master'))[:COMMITS_TO_PRINT]
         for commit in commits:
             print_commit(commit)
